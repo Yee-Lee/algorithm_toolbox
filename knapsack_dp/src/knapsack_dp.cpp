53ac501d680bf952ec9f029bc753cde1ca5419c1
yee-lee <yee.lee@hpe.com>
2019-12-12 14:00:53 +0800

    knapsack dp cpp solution

--- knapsack_dp/src/knapsack_dp.cpp
+++ knapsack_dp/src/knapsack_dp.cpp
@@ -1,30 +1,75 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
+using std::vector;
+using std::max;
 
-int main(void){
+/*
+  The value of items is same as its weight.
+ 
+      Capacity  0 1 2 3 4...W
+items  -------------------------------
+ idx  |
+  0   |  values of the whole knapsack
+  1   |
+  2   |
+  3   |
+(The items under consideration. Taken or not taken cannot be seen in this table.)
+*/
 
-  int A, B;
-  cin >> A >> B;
-
-  int m, n, r;
-  m = n = r = 0;
-  if( A > B){
-    m = A;
-    n = B;
-  }else{
-    m = B;
-    n = A;
+int optimal_weight(int W, const vector<int> &w, vector<int> &V) {
+  //write your code here
+  int **D;
+  D = new int* [w.size()];
+  for(int i=0; i<(int)w.size(); i++){
+    D[i] = new int[W+1];
+    D[i][0] = 0; // initialization
   }
 
-  do {
-    r = m % n;
-    if(r == 0)
-      break;
-    m = n;
-    n = r;
-  }while(true);
+  for(int i=0; i<(int)w.size(); i++){
+    int w_i = w[i];
+    int v_i = V[i];
+    for(int c=1; c<W+1; c++){
+      int v = 0;
+      if(c >= w_i){
+        if(i>0){
+          v = max(D[i-1][c], D[i-1][c-w_i]+v_i);
+        }else{
+          v = v_i;
+        }
+      }else{ // must not pick i up
+        v = (i>0)? D[i-1][c]: 0;
+      }
+      D[i][c] = v;
+    }
+  }
 
-  cout << n << endl;
-  return 0;
+/*
+  for(int i=0; i<(int)w.size(); i++){
+    for(int j=0; j<W+1; j++)
+      std::cout<<D[i][j]<<" ";
+    std::cout<<std::endl;
+  }
+*/
+  
+  int ret = D[(int)w.size()-1][W];
+  for(int i =0; i<(int)w.size(); i++)
+    delete [] D[i];
+  delete [] D;
+
+  return ret;
+}
+
+int main() {
+  int n, W;
+  std::cin >> W >> n;
+  vector<int> w(n);
+  for (int i = 0; i < n; i++) {
+    std::cin >> w[i];
+  }
+  vector<int> V(n);
+  for(int i=0 ; i<n; i++)
+  std::cin >> V[i]; 
+  std::cout << optimal_weight(W, w, V) << '\n';
 }
